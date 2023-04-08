e4b9585e4a10d4051315e9ce9485be3c4c5b8f41
emekachineyeze@gmail.com <emekakingsley1804@gmail.com>
2023-04-08 19:19:03 +0100

    third commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,16 +15,47 @@ const Home = () => {
   };
 
   return (
-    <div className="all" style={{ backgroundRepeat:"no-repeat", backgroundSize:"cover",backgroundImage: "url(/icons/house_635VI.jpg)", height: "100vh", width: "100%" }}>
+    <div className="all" style={{ backgroundRepeat:"no-repeat", backgroundSize:"cover",backgroundImage: "url(/icons/house_635VI.jpg)", height: "100%", width: "100%" }}>
       <Navbar />
       <br/> 
 
-      <div className="entry" style={{ backgroundRepeat:"no-repeat", backgroundImage: "url(/icons/gtcologo.png)", marginLeft:"10px" , height: "200px", width: "250px" }}></div>
+      <div className="entry" style={{ backgroundRepeat:"no-repeat", backgroundImage: "url(/icons/GTworldicon.png)", marginLeft:"40px" , height: "350px", width: "400px" }}></div>
+      <h3>GTWorld</h3>
        <br/> 
-       <div>
-        <img src="./icons/" alt=""/>
+      <div className="prelogin_icons">
+        <section>
+          <div>
+            <img src="./icons/QuickBalanceIcon.png" alt=""/>
+            <h5>Quick Balance</h5>
+          </div>
+          <div>
+            <img src="./icons/QuickTransferIcon.png" alt=""/>
+            <h5>Quick Transfer</h5>
+          </div>
+          <div>
+            <img src="./icons/QuickAirtimeIcon.png" alt=""/>
+            <h5>Quick Airtime</h5>
+          </div>
 
-       </div>
+        </section>
+
+        <section>
+          <div>
+            <img src="./icons/SoftTokenIcon.png" alt=""/>
+            <h5>Soft token</h5>
+          </div>
+          <div>
+            <img src="./icons/AccountManagerIcon.png" alt=""/>
+            <h5>Account Manager</h5>
+          </div>
+          <div>
+            <img src="./icons/OpenAccountIcon.png" alt=""/>
+            <h5>Open Account</h5>
+          </div>
+
+        </section>
+
+      </div>
        <br/>
 
       <div className="login">
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,13 +3,45 @@ import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
-    const [acc_number, setAccountNumber] = useState("222ffh");
-    const [password, setPassword] = useState("xfxddfgh");
+    const [acct_number, setAccountNumber] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleLogin = (e) => {
+    const handleChange = (e) => {
         e.preventDefault();
 
-    };
+        if (e.target.name === 'acct_number') {
+            setAccountNumber(e.target.value);
+        }
+        if (e.target.name === 'password') {
+            setPassword(e.target.value);
+        }
+
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        let data = { acct_number,password,
+
+        }
+
+        data = JSON.stringify(data);
+
+        let loginUser = async function (url, userData) {
+
+            let response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: userData
+            });
+
+        }
+
+
+
+    }
     
 
     return (
@@ -21,25 +53,19 @@ const Login = () => {
         <div className="login">
             <div className="container">
                 <div className="form">
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Enter Account number"
-                            onChange={(e) => setAccountNumber(e.target.value)}
-                        />
-                       <input
-                            type="text"
-                            placeholder="Password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+                    <form onSubmit={handleSubmit} action="" method="POST">
+                        <input type="text" placeholder="Enter Account number" onChange={handleChange}/>
+                        <input type="password" placeholder="Password" onChange={handleChange} />
+
+                        <button className="loginButton">
+                            <Link to="/home" className="link">Login</Link>
+                        </button>
                     
                     </form>
 
                 </div>
 
-                <button className="loginButton" onClick={handleLogin}>
-                    <Link to="/home" className="link">Login</Link>
-                </button>
+                
                 <div className="passwordlink">
                     <Link to="/" className="link1">Forgot password?</Link>
                     <Link to="/" className="link2">Sign Up</Link>
