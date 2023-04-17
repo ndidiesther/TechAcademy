0cc1df89577966545a53ef42151db86c5a65c57a
Esther Chibogu <estherchibogu@gmail.com>
2023-04-17 10:56:01 +0100

    worked on splash screen

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import '../Styles/Home.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "../Styles/Home.css";
+import { useNavigate } from "react-router-dom";
+import Logo from "../Assets/Budget_Icons/gtcologo.png"
 
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleButton1Click = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <div className="home">
-      <div className="entry" style={{ backgroundRepeat:"no-repeat", backgroundImage: "url(/Icons/gtcologo.png)", marginLeft:"200px", marginTop:"100px", height: "100%", width: "100%" }}>
-      <div className="login">
-        <button  className="login1" onClick={handleButton1Click}>Sign in to your account</button>
-      </div>
+      <div className="overlay"></div>
+      <div
+        className="entry"
+        // style={{
+        //   backgroundRepeat: "no-repeat",
+        //   backgroundImage: "url(/Icons/GTCO-60.png)",
+        //   height: "100%",
+        //   width: "40%",
+        // }}
+      >
+        < img className="logo" src={Logo} alt="" />
+        <div className="login">
+          <button className="login1" onClick={handleButton1Click}>
+            Sign in to your account
+          </button>
+        </div>
       </div>
     </div>
-
-   
-  )
-}
-
-  
+  );
+};
 
 export default Home;
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
-import {Link } from "react-router-dom";
-import '../Styles/Login.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Styles/Login.css";
+import Logo from "../Assets/Budget_Icons/gtcologo.png"
 
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  //const navigate = useNavigate()
 
-    //const navigate = useNavigate()
-  
-    /*const handleSubmit = async (event) => {
+  /*const handleSubmit = async (event) => {
       event.preventDefault();
       const response = await fetch('http://22a4-102-89-22-182.ngrok-free.app/api/UserIds', {
         method: 'POST',
@@ -35,43 +35,70 @@ import '../Styles/Login.css';
     };
     */
 
-
-    
-
-    return (
+  return (
     <div className="loginall">
+      <div className="overlay"></div>
       <div className="wrapper">
-      <div className="entry" style={{ backgroundRepeat:"no-repeat", backgroundImage: "url(/Icons/GTCO-60.png)", marginLeft:"260px", position:"absolute" ,top:"180px", height: "400px", width: "400px" }}></div>
-      
-        <div className="login">
-            <div className="container">
-                <div>
-                  <form>
-                    <div className="arrange">
-                      <input type="text" placeholder="Enter Account number" value={username} onChange={(event) => setUsername(event.target.value)}/>
-                      <input type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
-    
-                    <button className="loginButton">
-                      <Link to="/transaction" className="link1">Login</Link>
-                    </button>
-                    </div>
-                        
-                  </form>
-
-                </div>
-
-                
-                <div className="passwordlink">
-                  <div><Link to="/createuser" className="link1">Create new user</Link></div>
-                  <span><Link to="/" className="link2">Forgot password?</Link></span>
-                </div>
-            </div>
+        <div
+          className="entry-login"
+          
+          // style={{
+          //   backgroundRepeat: "no-repeat",
+          //   backgroundImage: "url(/Icons/GTCO-60.png)",
+          //   marginLeft: "260px",
+          //   position: "absolute",
+          //   top: "180px",
+          //   height: "400px",
+          //   width: "400px",
+          // }}
+        >
+            < img className="gtlg logo" src={Logo} alt="" />
         </div>
 
+        <div className="login">
+          <div className="container">
+            <div>
+              <form>
+                <div className="arrange">
+                  <input
+                    type="text"
+                    placeholder="Enter Account number"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+
+                  <button className="loginButton">
+                    <Link to="/transaction" className="link1">
+                      Login
+                    </Link>
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            <div className="passwordlink">
+              <div>
+                <Link to="/createuser" className="link3">
+                  Create New User
+                </Link>
+              </div>
+              <span>
+                <Link to="/" className="link2">
+                  Forgot Password?
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Login;
